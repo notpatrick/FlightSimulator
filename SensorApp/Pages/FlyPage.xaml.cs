382aba2d4a69b4911c9ab8da065334ea387e451c
Patrick <it.mueller@outlook.com>
2016-07-03 16:24:45 +0200

    Fix gap between tiles

--- SensorApp/Pages/FlyPage.xaml.cs
+++ SensorApp/Pages/FlyPage.xaml.cs
@@ -1,88 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Numerics;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Devices.Sensors;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Graphics.Display;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Navigation;
 using SensorApp.Annotations;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+namespace SensorApp {
+    public sealed partial class FlyPage : Page, INotifyPropertyChanged {
+        private readonly Inclinometer _inclinometer;
+        private readonly double _skyModificator;
+        private Image[] _groundImages;
+        private Image[] _skyImages;
+        private int _currentFrame;
 
-namespace SensorApp
-{
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class FlyPage : Page, INotifyPropertyChanged
-    {
-        private Inclinometer _inclinometer;
-        private Image[] backgroundImages;
-        private Image[] skyImages;
-        private int frame;
         public double Pitch { get; set; }
         public double Roll { get; set; }
         public double Yaw { get; set; }
         public double XSpeed { get; set; }
         public double YSpeed { get; set; }
-        
-        public FlyPage()
-        {
+
+        public FlyPage() {
             this.InitializeComponent();
             DisplayInformation.AutoRotationPreferences = DisplayOrientations.Landscape;
 
             YSpeed = 15;
             XSpeed = 0;
-            frame = 0;
-
+            _currentFrame = 0;
+            _skyModificator = .25;
             _inclinometer = Inclinometer.GetDefault();
-            if (_inclinometer != null)
-            {
+
+            if (_inclinometer != null) {
                 var minReportInterval = _inclinometer.MinimumReportInterval;
                 var reportInterval = minReportInterval > 16 ? minReportInterval : 16;
                 _inclinometer.ReportInterval = reportInterval;
                 _inclinometer.ReadingTransform = DisplayOrientations.Landscape;
                 _inclinometer.ReadingChanged += ReadingChanged;
             }
-
-            Loaded += delegate
-            {
+            // Initialize everything on Loaded
+            Loaded += delegate {
                 InitCanvas();
                 InitPlane();
-                InitBackground();
+                InitGround();
                 InitSky();
             };
         }
 
-        private async void ReadingChanged(object sender, InclinometerReadingChangedEventArgs e)
-        {
-            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-            {
-                if (frame%2 == 0)
-                {
+        private async void ReadingChanged(object sender, InclinometerReadingChangedEventArgs e) {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => {
+                // Render evere second frame
+                if (_currentFrame % 2 == 0) {
                     var reading = e.Reading;
                     UpdateView(reading);
+                    _currentFrame = 0;
                 }
-                frame++;
+                _currentFrame++;
             });
         }
 
-        private void UpdateView(InclinometerReading reading)
-        {
+        private void UpdateView(InclinometerReading reading) {
             Pitch = reading.PitchDegrees;
             Roll = reading.RollDegrees;
             Yaw = reading.YawDegrees;
@@ -94,14 +74,11 @@ namespace SensorApp
             UpdateBackground();
         }
 
-        private void UpdatePlane(double angle)
-        {
-            var rotation = new RotateTransform { Angle = angle };
-            Airplane.RenderTransform = rotation;
+        private void UpdatePlane(double angle) {
+            Airplane.RenderTransform = new RotateTransform {Angle = angle};
         }
 
-        private void InitPlane()
-        {
+        private void InitPlane() {
             double left = (PlaneArea.ActualWidth - Airplane.ActualWidth) / 2;
             Canvas.SetLeft(Airplane, left);
 
@@ -109,99 +86,88 @@ namespace SensorApp
             Canvas.SetTop(Airplane, top);
         }
 
-        private void InitCanvas()
-        {
-            //DrawArea.Clip = new RectangleGeometry()
-            //{
-            //    Rect = new Rect(0, 0, This.ActualWidth, This.ActualHeight)
-            //};
-            DrawArea.Height = This.ActualHeight;
-            DrawArea.Width = This.ActualWidth;
-            var proj = new PlaneProjection();
-            proj.RotationX = -86;
-            proj.GlobalOffsetY = 10;
-            proj.GlobalOffsetZ = 180;
-            DrawAreaBorder.Projection = proj;
+        private void InitCanvas() {
+            // Transform ground canvas so it looks 3D
+            var projection = new PlaneProjection {
+                RotationX = -86,
+                GlobalOffsetY = 10,
+                GlobalOffsetZ = 320
+            };
+            GroundDrawArea.Projection = projection;
+            GroundDrawArea.Height = This.ActualHeight;
+            GroundDrawArea.Width = This.ActualWidth;
         }
 
-
-        private void InitBackground()
-        {
-            var path = "ms-appx:///../Assets/MyImages/pattern.png";
-            var pathInverted = "ms-appx:///../Assets/MyImages/pattern-inverted.png";
-            var width = Math.Round(DrawArea.ActualWidth, MidpointRounding.AwayFromZero);
-            backgroundImages = new[]
-            {
-                new Image() {Source = new BitmapImage(new Uri(path)), Width = width, Height = width},
-                new Image() {Source = new BitmapImage(new Uri(pathInverted)), Width = width, Height = width},
-                new Image() {Source = new BitmapImage(new Uri(pathInverted)), Width = width, Height = width},
-                new Image() {Source = new BitmapImage(new Uri(path)), Width = width, Height = width}
+        private void InitGround() {
+            // Create array of ground images
+            var image = new BitmapImage(new Uri("ms-appx:///../Assets/MyImages/pattern.png"));
+            var imageInverted = new BitmapImage(new Uri("ms-appx:///../Assets/MyImages/pattern-inverted.png"));
+            var width = GroundDrawArea.ActualWidth;
+            _groundImages = new[] {
+                new Image() {Width = width, Height = width, Source = image},
+                new Image() {Width = width, Height = width, Source = imageInverted},
+                new Image() {Width = width, Height = width, Source = imageInverted},
+                new Image() {Width = width, Height = width, Source = image},
             };
-            int i = 0;
-            foreach (var backgroundImage in backgroundImages)
-            {
-                DrawArea.Children.Add(backgroundImage);
+            var i = 0;
+            foreach (var backgroundImage in _groundImages) {
+                GroundDrawArea.Children.Add(backgroundImage);
                 Canvas.SetZIndex(backgroundImage, -99);
-                ResetBackground(i);
+                InitGroundImage(i);
                 i++;
             }
         }
 
-        private void InitSky()
-        {
-            var path = "ms-appx:///../Assets/MyImages/big-sky.png";
+        private void InitSky() {
+            // Create array of sky images
+            var image = new BitmapImage(new Uri("ms-appx:///../Assets/MyImages/big-sky.png"));
             var areaHeight = SkyDrawArea.ActualHeight;
-            var height = Math.Round(areaHeight/2 );
-            var width = Math.Round(height/270*1728, MidpointRounding.AwayFromZero);
-            skyImages = new[]
-            {
-                new Image() {Source = new BitmapImage(new Uri(path)), Height = height, Width = width},
-                new Image() {Source = new BitmapImage(new Uri(path)), Height = height, Width = width},
+            var height = areaHeight / 2;
+            var width = height / 270 * 1728;
+            _skyImages = new[] {
+                new Image() {Height = height, Width = width, Source = image},
+                new Image() {Height = height, Width = width, Source = image},
             };
-            int i = 0;
-            foreach (var skyImage in skyImages)
-            {
+            var i = 0;
+            foreach (var skyImage in _skyImages) {
                 SkyDrawArea.Children.Add(skyImage);
                 Canvas.SetZIndex(skyImage, -90);
-                ResetSky(i);
+                InitSkyImage(i);
                 i++;
             }
         }
 
-        private void UpdateBackground()
-        {
-            XSpeed = Math.Round(HorizontalSpeed(Roll));
+        private void UpdateBackground() {
+            // Calculate XSpeed
+            XSpeed = Math.Round(GrowthFunction(Roll), 1);
             OnPropertyChanged(nameof(XSpeed));
-            foreach (var backgroundImage in backgroundImages)
-            {
-                var top = Canvas.GetTop(backgroundImage);
-                var left = Canvas.GetLeft(backgroundImage);
+            // Update each ground image based on XSpeed
+            foreach (var groundImage in _groundImages) {
+                var top = Canvas.GetTop(groundImage);
+                var left = Canvas.GetLeft(groundImage);
                 var newTop = top + YSpeed;
                 var newLeft = left;
 
-                if (Roll > 0)
-                {
+                if (Roll > 0) {
                     newLeft = left + XSpeed;
-                } else if (Roll < 0)
-                {
+                }
+                else if (Roll < 0) {
                     newLeft = left - XSpeed;
                 }
 
-                PositionInCanvas(backgroundImage, newLeft, newTop);
-                CheckBackgroundOutOfBound(backgroundImage);
+                PositionInCanvas(groundImage, newLeft, newTop);
+                CheckGroundImageBounds(groundImage);
             }
-            foreach (var skyImage in skyImages)
-            {
+            // Update each sky image based on XSpeed
+            foreach (var skyImage in _skyImages) {
                 var left = Canvas.GetLeft(skyImage);
                 var newLeft = left;
 
-                if (Roll > 0)
-                {
-                    newLeft = left + XSpeed;
+                if (Roll > 0) {
+                    newLeft = left + XSpeed * _skyModificator;
                 }
-                else if (Roll < 0)
-                {
-                    newLeft = left - XSpeed;
+                else if (Roll < 0) {
+                    newLeft = left - XSpeed * _skyModificator;
                 }
 
                 PositionInCanvas(skyImage, newLeft, 0);
@@ -209,65 +175,51 @@ namespace SensorApp
             }
         }
 
-        private double HorizontalSpeed(double x)
-        {
-            var max = 15;
-            var growthConst = -.025;
-            x = Math.Abs(x);
-
-            return max - max*Math.Pow(Math.E, growthConst*x);
-        }
-
-        private void CheckBackgroundOutOfBound(Image image)
-        {
-            var width = DrawArea.ActualWidth;
-            var height = DrawArea.ActualHeight;
+        private void CheckGroundImageBounds(Image image) {
+            // Move ground image to opposite side if it leaves screen
+            var width = GroundDrawArea.ActualWidth;
+            var imgWidth = image.ActualWidth;
+            var height = imgWidth;
 
             var top = Canvas.GetTop(image);
             var left = Canvas.GetLeft(image);
 
-            if (left > width - XSpeed)
-            {
-                Canvas.SetLeft(image, -width + XSpeed);
-            } else if (left < -width + XSpeed)
-            {
-                Canvas.SetLeft(image, width - XSpeed);
+            if (left > width) {
+                Canvas.SetLeft(image, left - 2 * imgWidth);
+            }
+            else if (left < -width) {
+                Canvas.SetLeft(image, left + 2 * imgWidth);
             }
 
-            if (top > width)
-            {
-                Canvas.SetTop(image, -width);
-            } else if (top < -width)
-            {
-                Canvas.SetTop(image, width);
+            if (top > width) {
+                Canvas.SetTop(image, top - 2 * height);
+            }
+            else if (top < -width) {
+                Canvas.SetTop(image, top + 2 * height);
             }
         }
 
-        private void CheckSkyOutOfBound(Image skyImage)
-        {
+        private void CheckSkyOutOfBound(Image skyImage) {
+            // Move sky image to opposite side if it leaves screen
             var width = skyImage.Width;
             var left = Canvas.GetLeft(skyImage);
 
-            if (left > width - XSpeed)
-            {
-                Canvas.SetLeft(skyImage, - width + XSpeed);
-            } else if (left < - width + XSpeed)
-            {
-                Canvas.SetLeft(skyImage, width - XSpeed);
+            if (left > width) {
+                Canvas.SetLeft(skyImage, left - 2 * width);
+            }
+            else if (left < -width) {
+                Canvas.SetLeft(skyImage, left + 2 * width);
             }
         }
 
-
-        private void ResetBackground(int index)
-        {
+        private void InitGroundImage(int index) {
             double x, y;
-            var width = DrawArea.ActualWidth;
-            var height = DrawArea.ActualHeight;
-            switch (index)
-            {
+            var width = GroundDrawArea.ActualWidth;
+            var height = GroundDrawArea.ActualHeight;
+            switch (index) {
                 case 0:
-                    x = -width/2;
-                    y = -width + height/2;
+                    x = -width / 2;
+                    y = -width + height / 2;
                     break;
                 case 1:
                     x = width / 2;
@@ -286,21 +238,18 @@ namespace SensorApp
                     y = 0;
                     break;
             }
-            PositionInCanvas(backgroundImages[index], x, y);
+            PositionInCanvas(_groundImages[index], x, y);
         }
 
-        private void ResetSky(int index)
-        {
+        private void InitSkyImage(int index) {
             double x, y;
-            var width = skyImages[index].ActualWidth;
-            switch (index)
-            {
+            switch (index) {
                 case 0:
                     x = 0;
                     y = 0;
                     break;
                 case 1:
-                    x = width;
+                    x = _skyImages[0].ActualWidth;
                     y = 0;
                     break;
                 default:
@@ -308,23 +257,33 @@ namespace SensorApp
                     y = 0;
                     break;
             }
-            PositionInCanvas(skyImages[index], x, y);
+            PositionInCanvas(_skyImages[index], x, y);
         }
 
-        private static void PositionInCanvas(Image element, double x, double y)
+        private static double GrowthFunction(double x)
         {
+            // Limited growth function to calculate XSpeed from roll
+            const int max = 15;
+            const double growthConst = -.025;
+            x = Math.Abs(x);
+
+            return max - max * Math.Pow(Math.E, growthConst * x);
+        }
+
+        private static void PositionInCanvas(UIElement element, double x, double y) {
             Canvas.SetLeft(element, x);
             Canvas.SetTop(element, y);
         }
 
         #region PropertyChanged
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         [NotifyPropertyChangedInvocator]
-        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null) {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
         #endregion
     }
 }
