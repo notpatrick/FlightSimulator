16560fc8cc402b2a01ebb879662e9833f587a2f4
Patrick <it.mueller@outlook.com>
2016-07-06 11:38:02 +0200

    Remove test pages

--- SensorApp/Pages/MainPage.xaml.cs
+++ SensorApp/Pages/MainPage.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using Windows.Graphics.Display;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using SensorApp.Pages;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -18,21 +17,6 @@ namespace SensorApp
 
             // Force landscape orientation
             DisplayInformation.AutoRotationPreferences = DisplayOrientations.Landscape;
-        }
-
-        private void AccelerometerButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(AccelerometerPage));
-        }
-
-        private void InclinometerButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(InclinometerPage));
-        }
-
-        private void OrientationButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(OrientationPage));
         }
 
         private void FlyButton_Click(object sender, RoutedEventArgs e)
